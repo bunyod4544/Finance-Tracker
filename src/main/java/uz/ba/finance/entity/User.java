ee68c662c4ed6a91173d95d4a6d152ce752afe3e
User <bunyod4544>
2023-07-04 23:33:50 +0500

    entities are done

--- src/main/java/uz/ba/finance/entity/User.java
+++ src/main/java/uz/ba/finance/entity/User.java
@@ -1,7 +1,34 @@
 package uz.ba.finance.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 /**
  * @author Bunyod on 04 июль 2023 at 23:05
  */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users", schema = "public")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
 }
